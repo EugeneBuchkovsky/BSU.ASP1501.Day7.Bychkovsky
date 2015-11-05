e674134359ca2dade15e6f7363b7554a29f3e858
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-11-05 18:13:06 +0200

    Task3

---
+++ Task3.Test/SearchTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3;
+
+namespace Task3.Test
+{
+    [TestClass]
+    public class SearchTest
+    {
+        [TestMethod]
+        public void BinarySearch_12345_5_result4()
+        {
+            int[] array = {8,9,10,11,12};
+            int item = 12;
+
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, array.BinarySearch<int>(item, null));
+        }
+
+        [TestMethod]
+        public void BinarySearch_stringArray_result2()
+        {
+            string[] array = { "ready","stady","to","go" };
+            string item = "to";
+
+            int expectedResult = 2;
+
+            Assert.AreEqual(expectedResult, array.BinarySearch<string>(item, null));
+        }
+    }
+}
---
+++ Task3/Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class Search
+    {
+        /// <summary>
+        /// method of binary search.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">Source Array</param>
+        /// <param name="item">Search item</param>
+        /// <returns>Index element in array.</returns>
+        public static int BinarySearch<T>(this T[] source, T item, IComparer<T> comparer)
+        {
+            if (item == null)
+                throw new ArgumentNullException();
+            if (comparer == null)
+                comparer = Comparer<T>.Default;
+
+            int left = 0, rigth = source.Length - 1, middle;
+
+            while (left <= rigth)
+            {
+                middle = (rigth - left) / 2 + left;
+
+                if (comparer.Compare(source[middle], item) == 0)
+                    return middle;
+                if (comparer.Compare(source[middle], item) > 0)
+                    rigth = middle - 1;
+                else
+                    left = middle + 1;
+            }
+            return -1;
+        }
+
+        public static int BinarySearch<T>(this T[] source, T item, Comparison<T> comparer)
+        {
+            return source.BinarySearch<T>(item, Comparer<T>.Create(comparer));
+        }
+    }
+}
